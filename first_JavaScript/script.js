bf135af0336b43111981ae1b0067bc82fc40de73
JordiPV <88150345+JorPV@users.noreply.github.com>
2021-11-20 19:00:09 +0100

    Add files via upload

---
+++ first_JavaScript/script.js
@@ -0,0 +1,18 @@
+console.log('Hello Winc Academy');
+let name = 'Jordi'; 
+console.log(name); 
+// let if;
+// console.log(if);
+// It is not possible to create a variable called if, because if is a keyword. 
+let addition = 11 + 4;
+console.log(addition);
+let subtraction = 20 - 16;
+console.log(subtraction);
+let multiplication = 3 * 4;
+console.log(multiplication);
+let division = 70 / 10;
+console.log(division);
+let modulus = 5 % 100;
+console.log(modulus);
+typeof 35;
+console.log(typeof 35);
